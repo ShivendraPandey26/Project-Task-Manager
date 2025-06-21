2fa618b8b11e736997b30a77272da78db23ddbee
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2025-06-22 02:25:43 +0530

    Add WorkspaceIdLayout component for workspace-specific layout handling

---
+++ app/(protected)/workspace/[workspaceId]/layout.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { getUserWorkspace } from "../../data/workspace/get-user-workspace";
+import { redirect } from "next/navigation";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebarContainer } from "@/components/sidebar/app-sidebar-container";
+
+interface Props {
+  children: React.ReactNode;
+  params: Promise<{ workspaceId: string }>;
+}
+
+const WorkspaceIdLayout = async ({ children, params }: Props) => {
+  const { data } = await getUserWorkspace();
+  const { workspaceId } = await params;
+  // console.log({ workspaceId });
+
+  if (data?.onboardingCompleted && !data?.workspaces) {
+    redirect("/create-workspace");
+  } else if (!data?.onboardingCompleted) {
+    redirect("/onboarding");
+  }
+
+  return (
+    <SidebarProvider>
+      <div className="w-full h-screen flex bg-background">
+        <AppSidebarContainer data={data as any} workspaceId={workspaceId} />
+
+        <main className="w-full min-h-screen overflow-y-auto">
+          <div className="flex items-start">
+            <SidebarTrigger className="pt-3" />
+            {/* <Navbar
+              id={data?.id as string}
+              name={data?.name as string}
+              email={data?.email as string}
+              image={data?.image as string}
+            /> */}
+          </div>
+          <div className="p-0 md:p-4 pt-2">{children}</div>
+        </main>
+      </div>
+    </SidebarProvider>
+  );
+};
+
+export default WorkspaceIdLayout;
