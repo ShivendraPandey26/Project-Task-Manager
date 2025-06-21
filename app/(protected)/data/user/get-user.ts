60832def7cded4110dbf0ed65f0c4b4eb6aa2c62
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2025-06-22 02:26:25 +0530

    Add getUserById function for user retrieval by ID

---
+++ app/(protected)/data/user/get-user.ts
@@ -0,0 +1,24 @@
+import { db } from "@/lib/db";
+import { userRequired } from "./is-user-authenticated"
+
+export const getUserById = async () => {
+    try {
+        const { user } = await userRequired();
+
+        const data = await db.user.findUnique({
+            where: {
+                id: user?.id
+            },
+        });
+
+        return data;
+    } catch (error) {
+        console.log(error);
+        return {
+            success: false,
+            error: true,
+            message: "Failed to get user",
+            status: 500
+        }
+    }
+}
