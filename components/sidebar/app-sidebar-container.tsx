14808b4a6d8ff3320b13505e8637f8ab0236accb
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2025-06-22 02:26:38 +0530

    Add AppSidebar and AppSidebarContainer components for workspace project management

---
+++ components/sidebar/app-sidebar-container.tsx
@@ -0,0 +1,40 @@
+import { getWorkspaceProjectsByWorkspaceId } from "@/app/(protected)/data/project/get-workspace-projects";
+import { getUserById } from "@/app/(protected)/data/user/get-user";
+import { $Enums, User } from "@prisma/client";
+import AppSidebar from "./app-sidebar";
+import { ProjectProps, WorkspaceMembersProps } from "@/utils/types";
+
+export interface AppSidebarDataProps extends User {
+  workspaces: {
+    id: string;
+    name: string;
+    createdAt: Date;
+    userId: string;
+    workspaceId: string;
+    accessLevel: $Enums.AccessLevel;
+    workspace: {
+      name: string;
+    };
+  }[];
+}
+
+export const AppSidebarContainer = async ({
+  data,
+  workspaceId,
+}: {
+  data: AppSidebarDataProps;
+  workspaceId: string;
+}) => {
+  const { projects, workspaceMembers } =
+    await getWorkspaceProjectsByWorkspaceId(workspaceId);
+  const user = await getUserById();
+
+  return (
+    <AppSidebar
+      data={data}
+      projects={projects as unknown as ProjectProps[]}
+      workspaceMembers={workspaceMembers as unknown as WorkspaceMembersProps[]}
+      user={user as User}
+    />
+  );
+};
---
+++ components/sidebar/app-sidebar.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { AppSidebarDataProps } from "./app-sidebar-container";
+import { User } from "@prisma/client";
+import { ProjectProps, WorkspaceMembersProps } from "@/utils/types";
+import { Sidebar, SidebarGroupLabel, SidebarHeader } from "../ui/sidebar";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { Plus } from "lucide-react";
+import WorkspaceSelector from "./workspace-selector";
+
+const AppSidebar = ({
+  data,
+  projects,
+  workspaceMembers,
+  user,
+}: {
+  data: AppSidebarDataProps;
+  projects: ProjectProps[];
+  workspaceMembers: WorkspaceMembersProps[];
+  user: User;
+}) => {
+  return (
+    <>
+      <Sidebar collapsible="icon">
+        <SidebarHeader className="bg-background">
+          <div className="flex items-center">
+            <Avatar>
+              <AvatarImage
+                src={
+                  "https://media.istockphoto.com/id/1148287574/vector/icon-clipboard-checklist-or-document-with-checkmarck-with-text-in-flat-style.jpg?s=612x612&w=0&k=20&c=WllwemH3OGd84hLIUYpeMypITum0KKRijKtr5nqD5Gw="
+                }
+              />
+            </Avatar>
+            <SidebarGroupLabel>
+              <span className="text-xl font-bold">Taskly</span>
+            </SidebarGroupLabel>
+          </div>
+          <div className="flex justify-between mb-0">
+            <SidebarGroupLabel className="mb-2 text-sm font-semibold text-muted-foreground uppercase">
+              Workspace
+            </SidebarGroupLabel>
+
+            <Button asChild size={"icon"} className="size-5">
+              <Link href="/create-workspace">
+                <Plus />
+              </Link>
+            </Button>
+          </div>
+
+          <WorkspaceSelector workspaces={data.workspaces} />
+        </SidebarHeader>
+      </Sidebar>
+    </>
+  );
+};
+
+export default AppSidebar;
