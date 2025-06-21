19fe303ffe2a05bd221e7361d100cf7a2b54ae1d
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2025-06-22 02:24:51 +0530

    Add type definitions for workspace, project, and workspace members

---
+++ utils/types.ts
@@ -0,0 +1,47 @@
+import { $Enums, AccessLevel, Workspace } from "@prisma/client";
+
+export interface WorkspaceMembersProps extends Workspace {
+    user: {
+        id: string;
+        name: string;
+        email: string;
+        image?: string;
+    };
+    projectAccess: {
+        id: string;
+        hasAccess: boolean;
+        projectId: string;
+    }[];
+}
+
+
+export interface ProjectProps {
+    id: string;
+    name: string;
+    description?: string | null;
+    workspaceId: string;
+    members: {
+        id: string;
+        userId: string;
+        workspaceId: string;
+        accessLevel: AccessLevel;
+        createdAt: Date;
+        user: {
+            id: string;
+            name: string;
+            email: string;
+            image?: string;
+        }
+    }[];
+}
+
+export interface WorkspaceProps {
+    id: string;
+    createdAt: Date;
+    userId: string;
+    workspaceId: string;
+    accessLevel: $Enums.AccessLevel;
+    workspace: {
+        name: string;
+    }
+}
