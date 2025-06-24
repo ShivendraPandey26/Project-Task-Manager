2a8f80722675709320348d84759c1b921e2c43cd
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2025-06-25 02:43:09 +0530

    Add getProjectDetails function to retrieve project information and associated data

---
+++ app/(protected)/data/project/get-project-details.ts
@@ -0,0 +1,135 @@
+import { db } from "@/lib/db";
+import { userRequired } from "../user/is-user-authenticated";
+import { TaskStatus } from "@prisma/client";
+
+export const getProjectDetails = async (
+    projectId: string,
+    workspaceId: string
+) => {
+    try {
+
+        const { user } = await userRequired();
+
+        const [isUserMember, totalWorkspaceMembers] = await Promise.all([
+            db.workspaceMember.findUnique({
+                where: {
+                    userId_workspaceId: {
+                        userId: user?.id as string,
+                        workspaceId,
+                    },
+                },
+            }),
+            db.workspaceMember.count({
+                where: {
+                    workspaceId
+                },
+            }),
+        ]);
+
+
+        if (!isUserMember) {
+            throw new Error('User is not a member of the workspace');
+        }
+
+        const [project, comments] = await Promise.all([
+            db.project.findUnique({
+                where: {
+                    id: projectId,
+                },
+                include: {
+                    projectAccess: {
+                        include: {
+                            workspaceMember: {
+                                include: {
+                                    user: {
+                                        select: {
+                                            id: true,
+                                            name: true,
+                                            image: true
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                    },
+                    tasks: {
+                        include: {
+                            assignedTo: {
+                                select: {
+                                    id: true,
+                                    name: true,
+                                    image: true,
+                                },
+                            },
+                            project: {
+                                select: {
+                                    id: true,
+                                    name: true,
+                                },
+                            }
+                        }
+                    },
+                    activities: {
+                        include: {
+                            user: {
+                                select: {
+                                    id: true,
+                                    name: true,
+                                    image: true,
+                                },
+                            },
+                        },
+                        orderBy: {
+                            createdAt: 'desc',
+                        }
+                    }
+                },
+            }),
+            db.comment.findMany({
+                where: {
+                    projectId,
+                },
+                include: {
+                    user: {
+                        select: {
+                            id: true,
+                            name: true,
+                            image: true,
+                        },
+                    },
+                },
+                orderBy: {
+                    createdAt: 'desc',
+                },
+            }),
+        ]);
+
+        const tasks = {
+            total: project?.tasks.length,
+            completed: project?.tasks.filter((task) => task.status === TaskStatus.COMPLETED).length,
+            inProgress: project?.tasks.filter((task) => task.status === TaskStatus.IN_PROGRESS).length,
+            overdue: project?.tasks.filter((task) => task.status !== TaskStatus.COMPLETED && task.dueDate && new Date(task.dueDate) < new Date()).length,
+            items: project?.tasks,
+        }
+
+        return {
+            project: {
+                ...project,
+                members: project?.projectAccess?.map(access => access.workspaceMember),
+            },
+            tasks,
+            activities: project?.activities,
+            totalWorkspaceMembers,
+            comments
+        }
+
+    } catch (error) {
+        console.log(error);
+
+        return {
+            status: false,
+            message: 'Something went wrong',
+            error: true
+        }
+    }
+}
