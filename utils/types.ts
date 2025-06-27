4cc50e48c459cab0e59cf458886881faf76b4496
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2025-06-27 17:25:12 +0530

    Add ProjectTasksProps interface and taskStatusVariant for task management

--- utils/types.ts
+++ utils/types.ts
@@ -1,4 +1,4 @@
-import { $Enums, AccessLevel, WorkspaceMember } from "@prisma/client";
+import { $Enums, AccessLevel, Task, TaskStatus, WorkspaceMember } from "@prisma/client";
 
 export interface WorkspaceMembersProps extends WorkspaceMember {
     user: {
@@ -53,3 +53,32 @@ export interface CommentProps extends Comment {
         image: string;
     };
 }
+
+export interface ProjectTasksProps extends Task {
+    assignedTo: {
+        id: string;
+        name: string;
+        image?: string
+    };
+    project: {
+        id: string;
+        name: string;
+    };
+}
+
+export interface Column {
+    id: TaskStatus;
+    title: string;
+    tasks: ProjectTasksProps[];
+}
+
+export const taskStatusVariant = {
+    [TaskStatus.TODO]: "#eab308",
+    [TaskStatus.PENDING]: "#f97316",
+    [TaskStatus.IN_PROGRESS]: "#3b82f6",
+    [TaskStatus.COMPLETED]: "#22c55e",
+    [TaskStatus.ON_HOLD]: "#facc15",
+    [TaskStatus.IN_REVIEW]: "#a855f7",
+    [TaskStatus.BACKLOG]: "#ef4444",
+    [TaskStatus.CANCELLED]: "#6b7280",
+};
