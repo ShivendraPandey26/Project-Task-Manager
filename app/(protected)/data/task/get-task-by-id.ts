a0007b89ec1dc3132538c32ddfad654a13dd32ce
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2025-06-28 19:33:48 +0530

    Add getTaskById function with user access checks and task retrieval

---
+++ app/(protected)/data/task/get-task-by-id.ts
@@ -0,0 +1,110 @@
+import { db } from "@/lib/db";
+import { userRequired } from "../user/is-user-authenticated"
+
+export const getTaskById = async (
+    taskId: string,
+    workspaceId: string,
+    projectId: string
+) => {
+    const { user } = await userRequired();
+
+    const isUserMember = await db.workspaceMember.findUnique({
+        where: {
+            userId_workspaceId: {
+                userId: user?.id as string,
+                workspaceId,
+            },
+        },
+    });
+
+    if (!isUserMember) {
+        throw new Error('User is not a member of the workspace')
+    }
+
+    const projectAccess = await db.projectAccess.findUnique({
+        where: {
+            workspaceMemberId_projectId: {
+                workspaceMemberId: isUserMember.id,
+                projectId,
+            },
+        },
+    });
+
+    if (!projectAccess) {
+        throw new Error('User does not have access to the project')
+    }
+
+    const [task, comments] = await Promise.all([
+        db.task.findUnique({
+            where: {
+                id: taskId,
+            },
+            include: {
+                assignedTo: {
+                    select: {
+                        id: true,
+                        name: true,
+                        image: true,
+                    },
+                },
+                attachments: {
+                    select: {
+                        id: true,
+                        name: true,
+                        url: true,
+                    },
+                },
+                project: {
+                    include: {
+                        projectAccess: {
+                            include: {
+                                workspaceMember: {
+                                    include: {
+                                        user: {
+                                            select: {
+                                                id: true,
+                                                name: true,
+                                                image: true
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        }),
+
+        db.comment.findMany({
+            where: {
+                projectId,
+            },
+            include: {
+                user: {
+                    select: {
+                        id: true,
+                        name: true,
+                        image: true,
+                    },
+                },
+            },
+            orderBy: {
+                createdAt: 'desc'
+            },
+        }),
+    ]);
+
+    const project = {
+        ...task?.project,
+        members: task?.project.projectAccess.map((access) => access.workspaceMember)
+    }
+
+    return {
+        task: {
+            ...task,
+            project,
+        },
+        comments,
+    }
+}
