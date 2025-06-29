19b6d6b14f51bdfda2d2672df8907b3c931bd921
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2025-06-29 18:42:59 +0530

    Implement workspace management functions: update, reset invite code, and delete with member validation

--- app/actions/workspace.ts
+++ app/actions/workspace.ts
@@ -5,6 +5,8 @@ import { userRequired } from "../(protected)/data/user/is-user-authenticated";
 import { workspaceSchema } from "@/lib/schema";
 import { db } from "@/lib/db";
 import { generateInviteCode } from "@/utils/generate-invite-code";
+import { AccessLevel } from "@prisma/client";
+import { redirect } from "next/navigation";
 
 export const createNewWorkspace = async (data: CreateWorkspaceDataType) => {
     try {
@@ -27,7 +29,7 @@ export const createNewWorkspace = async (data: CreateWorkspaceDataType) => {
             }
         });
 
-        return {data: res}
+        return { data: res }
 
     } catch (error) {
         console.log({ "error while creating workspace": error });
@@ -40,3 +42,97 @@ export const createNewWorkspace = async (data: CreateWorkspaceDataType) => {
     }
 
 };
+
+
+export const updateWorkspace = async (
+    workspaceId: string,
+    data: CreateWorkspaceDataType,
+) => {
+    const { user } = await userRequired();
+
+    const validatedData = workspaceSchema.parse(data);
+
+    const isUserAMember = await db.workspaceMember.findUnique({
+        where: {
+            userId_workspaceId: {
+                userId: user?.id as string,
+                workspaceId: workspaceId,
+            },
+        },
+    });
+
+    if (!isUserAMember) {
+        throw new Error("User is not a member of this workspace.");
+    }
+
+    const updatedWorkspace = await db.workspace.update({
+        where: {
+            id: workspaceId,
+        },
+        data: {
+            name: validatedData.name,
+            description: validatedData.description || "",
+        },
+    });
+
+    return { success: true };
+};
+
+export const resetWorkspaceInviteCode = async (workspaceId: string) => {
+    const { user } = await userRequired();
+
+    const isUserAMember = await db.workspaceMember.findUnique({
+        where: {
+            userId_workspaceId: {
+                userId: user?.id as string,
+                workspaceId: workspaceId,
+            },
+        },
+    });
+
+    if (!isUserAMember) {
+        throw new Error("User is not a member of this workspace.");
+    }
+
+    await db.workspace.update({
+        where: {
+            id: workspaceId,
+        },
+        data: {
+            inviteCode: generateInviteCode(),
+        },
+    })
+
+
+    return {
+        success: true
+    }
+}
+export const deleteWorkspace = async (workspaceId: string) => {
+    const { user } = await userRequired();
+
+    const isUserAMember = await db.workspaceMember.findUnique({
+        where: {
+            userId_workspaceId: {
+                userId: user?.id as string,
+                workspaceId: workspaceId,
+            },
+        },
+    });
+
+    if (!isUserAMember) {
+        throw new Error("User is not a member of this workspace.");
+    }
+
+    if (isUserAMember && isUserAMember.accessLevel !== AccessLevel.OWNER) {
+        throw new Error("Only the owner of the workspace can delete it.");
+    }
+
+    await db.workspace.delete({
+        where: {
+            id: workspaceId,
+        },
+    })
+
+    redirect("/workspace")
+}
