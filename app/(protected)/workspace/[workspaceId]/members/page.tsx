36c5cff0fd20b06242a8943a5721953a312a2dfa
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2025-11-02 23:27:41 +0530

    Add initial members page with placeholder content

---
+++ app/(protected)/workspace/[workspaceId]/members/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="h-[70vh] flex justify-center items-center">
+      <h1 className="text-3xl text-muted-foreground">
+        This page is in progress and will be live shortly...!
+      </h1>
+    </div>
+  );
+};
+
+export default page;
